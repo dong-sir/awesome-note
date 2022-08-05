a5959cae2c096c1222573b025a65a21cc7dbdeb3
dongsir <790269286@qq.com>
2022-08-05 17:35:29 +0800

    获取N天前

--- Go/time.go
+++ Go/time.go
@@ -2,5 +2,10 @@
 
 
 
-// 时间戳文件名称
+// 当前时间格式化
 time.Now().Format("2006-01-02-15-04-05")
+
+// 7 天前
+const DaysToKeep = 7
+updateTime := time.Now().AddDate(0, 0, -DaysToKeep).Unix()
+fmt.Println(updateTime)
